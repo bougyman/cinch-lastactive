56f80ffb7e3184e274c7358e4ce3b043e52fd39d
Brian Haberer <bhaberer@gmail.com>
2014-06-11 18:41:54 -0700

    Working version

--- lib/cinch/plugins/lastactive.rb
+++ lib/cinch/plugins/lastactive.rb
@@ -2,30 +2,30 @@ require 'cinch'
 require 'cinch/toolbox'
 
 module Cinch::Plugins
-    class LastActive
-      include Cinch::Plugin
+  class LastActive
+    include Cinch::Plugin
 
-      listen_to :join, method: :join_respond
-      listen_to :channel, method: :update_time
+    listen_to :join, method: :join_respond
+    listen_to :channel, method: :update_time
 
-      def initialize(*args)
-        @time = Time.now
-      end
+    def initialize(*args)
+      super
+      @time = Time.now
+    end
 
-      def join_respond(m)
-        m.reply "Hello, #{m.user.nick}. The last activity was #{time_passed}."
-      end
+    def join_respond(m)
+      return if m.user.nick == @bot.nick
+      m.reply "Hello, #{m.user.nick}. The last activity was #{time_passed} ago."
+    end
 
-      def update_time(m)
-        @time = Time.now
-        debug 'TIME'
-      end
+    def update_time(m)
+      @time = Time.now
+    end
 
-      private
+    private
 
-      def time_passed
-        Cinch::Toolbox.time_format(@time - Time.now)
-      end
+    def time_passed
+      Cinch::Toolbox.time_format(Time.now - @time)
     end
   end
 end
